28755457e8f31b7b436379d8b6ce4eb0b1b5b491
JiříKučera <Kujiri52@gmail.com>
2023-01-17 12:42:51 +0100

    first version

---
+++ death_calculator.py
@@ -0,0 +1,10 @@
+roky = int(input("Zadejte svůj věk: "))
+
+vek = 90 - roky
+mesice = vek * 12
+tyden = vek * 57
+den = vek * 365 
+
+
+print (f"Zbývá vám jestě\n {vek} let\n {mesice} měsíců\n {tyden} týdnů\n a {den} dnů\n do konce života s průměrným věkem 90 let.")
+
